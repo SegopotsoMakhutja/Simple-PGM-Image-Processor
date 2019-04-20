ab5e38cb0065b82684f6ac54160758b6b1a3a91a
MKHSEG001 <leketi.s@gmail.com>
2019-04-20 21:12:08 +0200

    created Matrix H file

---
+++ Matrix.hpp
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Matrix H file
+ * Author: Segopotso Makhutja
+ * Date: April 2019
+ * 
+ */
+
+#include "Image.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+using namespace std;
+namespace MKHSEG001
+{
+    class Matrix
+    {
+        public:
+            // cons + decons
+            Matrix(string file);
+            ~Matrix();
+
+            int N;
+            float* valPtr;
+            // get values from file
+            void fileVal(string file);
+    };
+} // namespace MKHSEG001
+
+#endif
