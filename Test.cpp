5b2cf6e0736c61d47136e911d7b836782ebcc83e
Segopotso <leketi.s@gmail.com>
2019-04-30 22:27:33 +0200

    testing files created

---
+++ Test.cpp
@@ -0,0 +1,17 @@
+/**
+ * 
+ * unit testing
+ * Author: Segopotso Makhutja
+ * Date: April 2019
+ * 
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "Image.hpp"
+#include "Matrix.hpp"
+#include "Ops.hpp"
+
+using namespace MKHSEG001;
+using namespace std;
+
